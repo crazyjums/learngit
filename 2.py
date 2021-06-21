2b3e14bd7823da5e793c030253ea1946cadc1715
crazyjums <crazyjums@gmail.com>
2021-06-21 20:17:30 +0800

    merge manual

--- 2.py
+++ 2.py
@@ -4,10 +4,7 @@ def main():
     print("hello world.")
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     # this is in master branch
-=======
     # this is in topic branch
->>>>>>> topic
     main()
 
